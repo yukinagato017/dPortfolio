d8e7609d1f78a106fa324f12b2dbda396792f583
yukinagato <jecagustin017@gmail.com>
2020-07-03 17:47:23 +0800

    3rd commit

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -4,8 +4,7 @@
       <div class="pic" :style="contactVector"></div>
       <div class="form">
         <h1>Get in touch</h1>
-        <form name="contact" method="POST" data-netlify="true">
-           <input type="hidden" name="form-name" value="ask-question" />
+        <form name="contact" method="POST" netlify>
           <input type="text" name="name" placeholder="Name" required /><br />
           <input type="email" name="email" placeholder="Email" required /><br />
           <textarea name="message" placeholder="Message" required></textarea
